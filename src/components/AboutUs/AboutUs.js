3de387887d17cf8b8fc412b6ea160c4e9fe45870
At0m234 <61670155+At0m234@users.noreply.github.com>
2022-02-09 23:44:22 +0300

    fixed adaptive and font-size

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -4,9 +4,7 @@ function AboutUs (props) {
 	return (
 		<section className='about-us'>
 			<div className='about-us__content'>
-				<div className='about-us__container'>
-					<h2 className='about-us__title'>Who We Are</h2>
-				</div>
+				<h2 className='about-us__title'>Who We Are</h2>
 				<div className='about-us__container'>
 					<h3 className='about-us__subtitle'>Residential And Commercial, Builders And Contractors </h3>
 					<p className='about-us__text'>The Nord Construction is a full-service construction company, general contractor, design-builder and construction manager with office locations throughout the United States.</p>
